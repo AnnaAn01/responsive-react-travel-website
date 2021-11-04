ad11f9fee11ed0c23fe081aba3867cf4ead9b99c
Anna An <arzumanyan.an@gmail.com>
2021-11-04 16:53:56 +0400

    added some css and functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -17,12 +18,15 @@ function Navbar() {
     }
   };
 
-  window.addEventListener("resie", showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
+  window.addEventListener("resize", showButton);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
