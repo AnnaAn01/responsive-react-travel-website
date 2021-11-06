8b58a39f710531990c4dc3a251e38562cf5a03ae
Anna An <arzumanyan.an@gmail.com>
2021-11-06 17:00:40 +0400

    made some css changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/vide-2.mp4" autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1>SOME TEXT HERE</h1>
+      <p>Call to action here</p>
       <div className="hero-btns">
         <Button
           className="btns"
