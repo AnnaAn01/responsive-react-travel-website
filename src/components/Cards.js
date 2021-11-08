7e8486db25f6a97f163552cb590159402589ef15
Anna An <arzumanyan.an@gmail.com>
2021-11-08 17:37:36 +0400

    Added new images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/img-9.jpg"
+              src="images/img-13.jpg"
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="images/img-2.jpg"
+              src="images/img-14.jpg"
               text="Travel through the island of Bali"
               label="Luxury"
               path="/services"
